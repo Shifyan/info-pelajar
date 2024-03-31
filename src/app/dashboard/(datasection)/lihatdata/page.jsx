3bf0489fc1b895b5f82d4277d9b0aa47bc297817
Shifyannn <asifyan01@gmail.com>
2024-03-31 21:42:54 +0700

    Penambahan Fitur

--- src/app/dashboard/(datasection)/lihatdata/page.jsx
+++ src/app/dashboard/(datasection)/lihatdata/page.jsx
@@ -1,5 +1,7 @@
 async function fetchData() {
-  const res = await fetch('http://localhost:3000/api/pelajar')
+  const res = await fetch('http://localhost:3000/api/pelajar', {
+    cache: 'no-store'
+  })
   return res.json()
 }
 
@@ -11,8 +13,21 @@ export default async function LihatData() {
   return (
     <>
       <ul>
+        <li>{data.data[0].first_name}</li>
         {data.data.map(e => {
-          return <li key={e.id}>{e.first_name}</li>
+          return (
+            <li key={e.id}>
+              <div className='p-2 border rounded-lg grid grid-cols-5 gap-5'>
+                <h1>No. {e.id}</h1>
+                <h1>
+                  Nama: {e.first_name} {e.last_name}
+                </h1>
+                <h1>{e.email}</h1>
+                <h1>{e.gender}</h1>
+                <h1>{e.city}</h1>
+              </div>
+            </li>
+          )
         })}
       </ul>
     </>
